4dff8ae0d6094a19af8a85906423d903b4d30906
Hari Shankar <harishankarn04@gmail.com>
2024-12-12 13:39:12 +0530

    update

--- C_Programming/basic/trial.c
+++ C_Programming/basic/trial.c
@@ -1,17 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h> 
 
+// Defining car_node struct
 typedef struct car_node{
 	char *data; 
 	struct car_node *next_car;
 
 }car_node;
 
+// Joining the nodes into a singly linked list 
 car_node *create_car_node(char *data){
 	car_node *new_car_node=( car_node *)malloc(sizeof( car_node));
     new_car_node->data=data;
 
 	return new_car_node;
+}
+
+void list_car_node(car_node *node_head){
+	printf("list:\n");
+    for( car_node *temp=node_head;temp!=NULL;temp=temp->next_car)
+        printf("%s\n",temp->data);
 }
 
 int main(){
@@ -20,9 +28,7 @@ int main(){
 	first->next_car=create_car_node("toyota");
 	first->next_car->next_car=create_car_node("nissan");
 
-	printf("list:\n");
-	for( car_node *temp=first;temp!=NULL;temp=temp->next_car)
-		printf("%s\n",temp->data);
+	list_car_node(first);
 
 	return 0;
 }
