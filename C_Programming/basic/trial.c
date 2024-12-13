52a042cb5c12f801db367fc0b49f0cfbfcf0bb37
Hari Shankar <harishankarn04@gmail.com>
2024-12-13 15:50:18 +0530

    update

--- C_Programming/basic/trial.c
+++ C_Programming/basic/trial.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h> 
+#include <unistd.h> 
 
 // Defining car_node struct
 typedef struct car_node{
@@ -8,22 +9,51 @@ typedef struct car_node{
 
 }car_node;
 
-// Joining the nodes into a singly linked list 
+// Follow C.R.U.D principle
+// (Create)
+// Creates a node 
 car_node *create_car_node(char *carName){
 	car_node *new_car_node=( car_node *)malloc(sizeof( car_node));
 
 	new_car_node->data=carName;
 	new_car_node->next_car=NULL;	
-	
+
 	return new_car_node;
 }
 
+// (Read)
 // Function to list all the cars
 void read_car_node(car_node *head){
+	printf("--------------------\n");
+	printf("Cars in the list are:\n");
 	for( car_node *temp=head;temp!=NULL;temp=temp->next_car)
         printf("%s\n",temp->data);
+	printf("--------------------\n\n");
+}	
+
+
+//(Update)
+// Function to join a node at the end of list
+car_node* append(car_node *head, char* new_data){
+	car_node *new_car_node=create_car_node(new_data);
+
+	car_node *last = head;
+
+	if (head == NULL)
+    {
+       return new_car_node;
+    }  
+
+	while(last->next_car!=NULL){
+		last=last->next_car;
+	}
+
+	// last = last->next_car;
+	last->next_car = new_car_node;
+	return head;
 }
 
+// (Delete)
 // Function to delete car
 car_node* delete_car_node(car_node* head)
 {
@@ -52,48 +82,88 @@ car_node* delete_car_node(car_node* head)
     return head;
 }
 
+// Main function
 int main(){
-	/*first->next_car=create_car_node("toyota");
-	first->next_car->next_car=create_car_node("nissan");
-	first->next_car->next_car->next_car=create_car_node("mazda");
-	first->next_car->next_car->next_car->next_car=create_car_node("subaru");*/
 
 	car_node *first=NULL;
 
 	while(1){
-		printf("1.Create/Add \n");
-		printf("2.Delete last entry\n");
+
+		system("clear");
+		printf("--------------------\n");
+		printf("1.Create new list\n");
+		printf("2.Add to existing list\n");
 		printf("3.Print \n");
+		printf("4.Delete last entry\n");
+		printf("--------------------\n");
 
 		int choice;
+		printf("Enter your choice:");
 		scanf("%d",&choice);
+
+
 
 		switch(choice){
 			case 1:{
+				printf("\nCreating new list\n");
 				char *car_name = (char *)malloc(sizeof(char));
+				if (car_name == NULL) {
+					printf("Memory not allocated.\n");
+					break;
+				}
 				printf("Enter car name:");
 				scanf("%s",car_name);
-				
 				first=create_car_node(car_name);
-			//	printf("%s\n",first);
-
-				//printf("%s\n",first->next_car->next_car);
-				//printf("%s\n",first->next_car->next_car->next_car);
 				break;
 			}
 			
 			case 2:{
-				printf("\nList after removing the last node: \n");
-				first = delete_car_node(first);
-				read_car_node(first);
-				break;
+				printf("\nAdding to existing list\n");
+				char *carName = (char *)malloc(sizeof(char));
+				if (carName == NULL) {
+					printf("Memory not allocated.\n");
+					break;
+				}
+				if (first == NULL) {
+					printf("List is empty. Please create a list first.\n");
+					sleep(2);
+					break;
+				}
+				printf("Enter car name:");
+				scanf("%s",carName);
+				append( first,carName);
+				break;	
 			}
 			
 			case 3:{
+				if (first == NULL) {
+					printf("List is empty. Please create a list first.\n");
+					sleep(2);
+					break;
+				}
 				read_car_node(first);
+				sleep(2);
 				break;
 			}
+
+			case 4:{
+				if (first == NULL) {
+					printf("List is empty. Please create a list first.\n");
+					sleep(2);
+					break;
+				}
+				printf("\nList after removing the last node: \n");
+				first = delete_car_node(first);
+				read_car_node(first);
+				sleep(2);
+				break;
+			}
+
+			default:{
+				printf("Invalid input");
+			}
 		}
+		
 	}
 	return 0;
 }
